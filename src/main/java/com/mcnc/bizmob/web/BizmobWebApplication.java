a855718f1b47939a935f64e08f0d2a09bcfe73e9
도현 <lisa6688@naver.com>
2025-08-19 13:31:27 +0900

    oci 설정 제외

--- src/main/java/com/mcnc/bizmob/web/BizmobWebApplication.java
+++ src/main/java/com/mcnc/bizmob/web/BizmobWebApplication.java
@@ -2,8 +2,15 @@ package com.mcnc.bizmob.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+
+import com.mcnc.bizmob.web.global.config.OCIStorageConfig;
 
 @SpringBootApplication
+@ComponentScan(excludeFilters = @ComponentScan.Filter(
+		  type = FilterType.ASSIGNABLE_TYPE, classes = { OCIStorageConfig.class }
+		))
 public class BizmobWebApplication {
 
 	public static void main(String[] args) {
